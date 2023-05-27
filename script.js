37bffe445d9105687f3074d8b98e67091127a42e
karthik-mohan23 <132973611+karthik-mohan23@users.noreply.github.com>
2023-05-27 15:44:42 +0530

    Enhanced UI and made calculator functional.

--- script.js
+++ script.js
@@ -14,52 +14,85 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
-
+// modulus
 function modulus(num1) {
   return num1 / 100;
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
+let previousValue = "";
+let currentValue = "";
+let operator = "";
 
 function operate(operator, num1, num2) {
+  console.log(operator(num1, num2));
   return operator(num1, num2);
 }
 
-//
+// calculator display
+let calculatorDisplay = document.querySelector(".display-value");
 
-let displayValue = document.querySelector(".display-value");
-let displayNumbers = "";
-
+// to get number
 const numberButtons = document.querySelectorAll(".number");
-numberButtons.forEach((numberButton) => {
-  numberButton.addEventListener("click", () => {
-    if (!displayNumbers) {
-      displayNumbers += numberButton.textContent;
-      displayValue.textContent = displayNumbers;
-    }
-    // else {
-    //   displayValue.textContent += `${firstNumber} ${operator} ${numberButton.textContent}`;
-    // }
-
-    // operate(operator, displayNumbers, num2);
+numberButtons.forEach((numberBtn) => {
+  numberBtn.addEventListener("click", () => {
+    currentValue += numberBtn.textContent;
+    calculatorDisplay.textContent = currentValue;
   });
 });
 
-const operations = document.querySelectorAll(".operator");
-
-operations.forEach((operation) => {
-  operation.addEventListener("click", () => {
-    // storing first number
-    firstNumber = displayNumbers;
-    console.log(firstNumber);
-    displayNumbers = `${displayNumbers} ${operation.textContent}`;
-    // storing operator
-    operator = operation.textContent;
-    console.log(operator);
-    displayValue.textContent = displayNumbers;
-    // resetting displayNumber to zero to store second number
-    displayNumbers = "";
+//to get operator
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach((operatorBtn) => {
+  operatorBtn.addEventListener("click", () => {
+    operator = operatorBtn.textContent;
+    previousValue = currentValue;
+    currentValue = "";
+    calculatorDisplay.textContent = previousValue + " " + operator;
+    console.log(previousValue + operator);
   });
+});
+
+// calculation
+const equalToButton = document.querySelector(".equal-to");
+equalToButton.addEventListener("click", () => {
+  previousValue = Number(previousValue);
+  currentValue = Number(currentValue);
+  switch (operator) {
+    case "+":
+      console.log(operator, previousValue, currentValue);
+      calculatorDisplay.textContent = operate(add, previousValue, currentValue);
+      break;
+    case "-":
+      console.log(operator, previousValue, currentValue);
+
+      calculatorDisplay.textContent = operate(
+        subtract,
+        previousValue,
+        currentValue
+      );
+      break;
+    case "×":
+      console.log(operator, previousValue, currentValue);
+      calculatorDisplay.textContent = operate(
+        multiply,
+        previousValue,
+        currentValue
+      );
+      break;
+    case "÷":
+      console.log(operator, previousValue, currentValue);
+      calculatorDisplay.textContent = operate(
+        divide,
+        previousValue,
+        currentValue
+      );
+      break;
+    case "%":
+      console.log(operator, previousValue);
+      calculatorDisplay.textContent = operate(modulus, previousValue);
+      break;
+    default:
+      console.log("Error");
+      break;
+  }
 });
